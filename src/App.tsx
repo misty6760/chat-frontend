3c36324bfac78b149ad67b3808794234dd6b5ed8
misty6760 <kingh0jun12@naver.com>
2025-05-11 17:56:25 +0900

    2025/5/11

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,47 @@
-import { createBrowserRouter, RouterProvider } from "react-router"
+import { createBrowserRouter, RouterProvider, Outlet } from "react-router";
 import Home from "./pages/Home";
+import Login from "./pages/Login";
+import Signup from "./pages/Signup";
 
 const App = () => {
   const router = createBrowserRouter([
     {
       path: "/",
-      element: <Home />,
+      element: (
+        <div>Root Layout<Outlet /></div>
+      ),
+      children: [
+        { index: true, element: <div> <Home /> </div> },
+        {
+          path: "auth",
+          children: [
+            {
+              path: "login",
+              element: <Login />,
+            },
+            {
+              path: "signup",
+              element: <Signup />,
+            },
+          ],
+        },
+        {
+          path: "chat",
+          element: (
+            <div>Chat Layout <Outlet /></div>
+          ),
+          children: [
+            {
+              path: "room",
+              element: <div>Chat Room Page</div>,
+            },
+            {
+              path: "roomId",
+              element: <div>Chat Room ID Page</div>,
+            },
+          ],
+        },
+      ],
     },
   ]);
   return <RouterProvider router={router} />;
